548ef6740b1408947004a22b46352408dd7d7292
Sam <samhum90@gmail.com>
2020-05-31 03:09:59 +0100

    uploading

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+const path = require('path');
+const serveStatic = require('serve-static');
+
+app.use(serveStatic(__dirname + "/dist"));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('Hr_requests');
+    const requestsCollection = db.collection('requests');
+    const requestsRouter = createRouter(requestsCollection);
+    app.use('/api/requests', requestsRouter);
+  })
+  .catch(console.err);
+
+app.listen(3000, function () {
+  console.log(`App running on port ${ this.address().port }`);
+});
