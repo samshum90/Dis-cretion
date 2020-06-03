ba4b1107db0e6da6ef347321b7bd9d1599caa771
Sam <samhum90@gmail.com>
2020-06-03 03:02:05 +0100

    deploy

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ const serveStatic = require('serve-static');
 // const history = require('connect-history-api-fallback')
 
 
-app.use(serveStatic(__dirname + "dist"));
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
 app.use(bodyParser.json());
 app.use(cors());
 // app.use(history())
