f10314c0806c939e1e14eae60d8ed4a5cd63d302
Sam <samhum90@gmail.com>
2020-06-03 04:17:12 +0100

    deploy

--- server.js
+++ server.js
@@ -8,26 +8,28 @@ const path = require('path');
 const serveStatic = require('serve-static');
 // const history = require('connect-history-api-fallback')
 
-
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/index.html'))
+})
 app.use(serveStatic(__dirname + "dist"));
 app.use(bodyParser.json());
 app.use(cors());
 // app.use(history())
 
 
-// MongoClient.connect('mongodb://35.192.137.226:3000')
-//   .then((client) => {
-//     const db = client.db('flexible_work');
-//     const requestsCollection = db.collection('response');
-//     console.log(requestsCollection)
-//     const requestsRouter = createRouter(requestsCollection);
-//     app.use('/', requestsRouter);
-//   })
-//   .catch(console.err);
+MongoClient.connect('mongodb://35.192.137.226:3000')
+  .then((client) => {
+    const db = client.db('flexible_work');
+    const requestsCollection = db.collection('response');
+    console.log(requestsCollection)
+    const requestsRouter = createRouter(requestsCollection);
+    app.use('/', requestsRouter);
+  })
+  .catch(console.err);
 
-//   app.engine('html', require('ejs').renderFile);
+  app.engine('html', require('ejs').renderFile);
 
-  var port = process.env.PORT || 80;
+  var port = process.env.PORT || 5000;
 app.listen(port, function () {
   console.log(`App running on port ${ this.address().port }`);
 });
